07cc7acbb405f360d93f1a370571261d566f4f0d
Rohit Sinha <rohit.sinhalala@gmail.com>
2018-07-17 10:34:20 +0530

    Intial version committed

---
+++ Bot Application2/Bot Application2/Dialogs/RootDialog.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Bot_Application2.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // calculate something for us to return
+            //int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            if(activity.Text.Contains("Hi") || activity.Text.Contains("hi"))
+            {
+                //await context.PostAsync("Hi i am a FAQ bot ");
+               
+                /*
+                 * Prompt dialog used to prompt a message to the user from the bot side.
+                 * */
+
+                PromptDialog.Text(
+                    context,
+                    callFaqService,
+                    "Hi, I am DFAQ a Domino's FAQ bot. So, let's get started",
+                    "Oops, some error occurred.Sorry for the inconvinence");
+                
+            }
+
+            //context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task callFaqService(IDialogContext context, IAwaitable<string> result)
+        {
+            var userAnswer = await result;
+
+            //connection to the qnaMaker api which contains the Knowledge base in the form of question and answer format
+
+            // use try and catch block
+
+            try
+            {
+                //subscription key for the QnA maker kb stored in the web config file.
+
+                var subscriptionkey = ConfigurationManager.AppSettings["QnASubscriptionKey"];
+
+                /*
+                 * knowledge base id used to connect to the kb in the qnamaker details can be found in the 
+                 * http call of qnamaker website
+                 **/
+                
+                var knowledgeBaseId = ConfigurationManager.AppSettings["QnAKnowledgeBaseId"];
+
+                // call the GetQnaResponse method of QnAMakerDialog
+
+                var query = new QnAMakerDialog.QnAMakerDialog().GetQnAResponse(userAnswer, subscriptionkey, knowledgeBaseId);
+
+                var response = query.ans.FirstOrDefault();
+
+                if (response != null && response.score >= 50.0)
+                    await context.PostAsync(response.ans);
+            }
+            
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Bot Application2/Bot Application2/QnAMakerDialog/QnAMakerDialog.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace QnAMakerDialog
+{
+    internal class QnAMakerDialog
+    {
+        public QnAMakerDialog()
+        {
+        }
+
+        internal QnAResult GetQnAResponse(string userAnswer, string subscriptionkey, string knowledgeBaseId)
+        {
+            var rString = String.Empty;
+
+            /**
+             * Build the uri
+             **/
+            var build = new UriBuilder($"https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/" + knowledgeBaseId + "/generateAnswer/");
+
+            /**
+             *  Add query to the header as the part of body
+             **/
+
+            var body = $"{{\"question\": \"{userAnswer}\"}}";
+
+            /**
+             * Sending the post request to API endpoint
+             **/
+
+            using (WebClient client = new WebClient())
+            {
+                //Set the encoding to UTF-8 encoding part 
+                client.Encoding = System.Text.Encoding.UTF8;
+
+                //Add the subscription key sent to the method to the header details
+
+                client.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionkey);
+                client.Headers.Add("Content-Type", "application/json");
+
+                rString = client.UploadString(build.Uri, body);
+            }
+
+            /**
+             * De-serialize the response recieved from the qnamaker api
+             **/
+
+            QnAResult res;
+
+            try
+            {
+                var t = JsonConvert.DeserializeObject(rString);
+                res = JsonConvert.DeserializeObject<QnAResult>(rString);
+                return res;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Unable to convert the JSON result");
+            }
+        }
+    }
+    public class QnAResult
+    {
+        public Answer[] ans { get; set; }
+
+        public class Answer
+        {
+            [JsonProperty(PropertyName = "answer")]
+            public string ans { get; set; }
+
+            [JsonProperty(PropertyName = "score")]
+            public double score { get; set; }
+        }
+    }
+}
